a4fa82e1d768b613c26a4b62a46d66d98c8a9a87
andfaulkner <andfaulkner@gmail.com>
2016-04-11 15:10:45 -0400

    users controller & view tests added

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -4,6 +4,14 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+		assert_template 'users/new' # ensure root path is rendered w/ the given view / template
+
+    assert_select "body > div > h1:first-child", "Sign up"
+    assert_select "title", full_title("Sign up") #recap: full_title appends '| Sample app' for you
+		assert_select "a[href=?]", help_path # test for presence of help_path in an a href tag
+		assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", contact_path
+		assert_select "a[href=?]", root_path, count: 2 #link to root_path should be present X2 
   end
 
 end
