e36c1bea5716cd723e8b912189cbb2efe05d53bd
andfaulkner <andfaulkner@gmail.com>
2016-04-11 05:55:38 -0400

    backtrace silencers, helpers

--- test/functional/static_pages_controller_test.rb
+++ test/functional/static_pages_controller_test.rb
@@ -6,25 +6,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home 										# makes a get request to the home (/home) action in /static_pages 
     assert_response :success  		# :success is a representation of a 200 OK HTTP status code
-    assert_select "title", "Home" # selects matching HTML tag, checks that contents matches 2nd arg ("Home")
+    assert_select "title", "Sample app" # selects matching HTML tag, checks that contents matches 2nd arg ("Home")
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "Help | Sample app"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "About | Sample app"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact"
+    assert_select "title", "Contact | Sample app"
   end
 
 end
